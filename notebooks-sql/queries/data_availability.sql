9832b101c88f412c7c33e23661051a601e89c72e
Jose Mari Angelo Abeleda <gioabeleda@Gios-Macbook-Air.local>
2023-11-08 20:25:11 +0800

    changed sql query to have year_month

--- notebooks-sql/queries/data_availability.sql
+++ notebooks-sql/queries/data_availability.sql
@@ -1,3 +1,14 @@
+CREATE OR REPLACE VIEW wind_sites.data_availability AS
+SELECT 
+    EXTRACT(YEAR FROM date_time) AS year,
+    EXTRACT(MONTH FROM date_time) AS month,
+	TO_CHAR(date_time, 'YYYY-FMMonth') as year_month,
+    COUNT(DISTINCT EXTRACT(DAY FROM date_time)) as days_count
+FROM 
+    wind_sites.upd_wind_site
+GROUP BY 1,2, TO_CHAR(date_time, 'YYYY-FMMonth')
+;
+
 -- CREATE OR REPLACE VIEW wind_sites.data_availability AS
 -- SELECT 
 --     a.year,
@@ -16,14 +27,3 @@
 -- ;
 
 -- DROP VIEW wind_sites.data_availability;
-
-CREATE OR REPLACE VIEW wind_sites.data_availability AS
-SELECT 
-    EXTRACT(YEAR FROM date_time) AS year,
-    EXTRACT(MONTH FROM date_time) AS month,
-    COUNT(DISTINCT EXTRACT(DAY FROM date_time)) as days_count
-FROM 
-    wind_sites.upd_wind_site
-GROUP BY 1,2
-;
-
