36463f16ade241ae0339141a56ac44cdeae72d78
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-11-09 20:08:17 +0800

    Monthly basic stats query

---
+++ notebooks-sql/queries/stats_monthly.sql
@@ -0,0 +1,11 @@
+SELECT  
+	TO_CHAR(date_time, 'YYYY-FMMonth') AS year_month,
+	MAX(wind_speed) AS montly_max_speed,
+	MIN(wind_speed) AS monthly_min_speed,
+	ROUND(AVG(wind_speed),3) AS montly_avg_speed
+FROM 
+    wind_sites.upd_wind_site
+GROUP BY
+	TO_CHAR(date_time, 'YYYY-FMMonth'),
+ORDER BY
+		year_month
