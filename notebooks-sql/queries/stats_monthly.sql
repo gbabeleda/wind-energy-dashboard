ca8b37f103b89981dffa95b47a12d896eb7f5b0e
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-11-09 20:09:25 +0800

    create view monthly stats

--- notebooks-sql/queries/stats_monthly.sql
+++ notebooks-sql/queries/stats_monthly.sql
@@ -1,11 +1,12 @@
+CREATE OR REPLACE VIEW wind_sites.stats_monthly AS 
 SELECT  
-	TO_CHAR(date_time, 'YYYY-FMMonth') AS year_month,
-	MAX(wind_speed) AS montly_max_speed,
-	MIN(wind_speed) AS monthly_min_speed,
-	ROUND(AVG(wind_speed),3) AS montly_avg_speed
+TO_CHAR(date_time, 'YYYY-FMMonth') AS year_month,
+MAX(wind_speed) AS montly_max_speed,
+MIN(wind_speed) AS monthly_min_speed,
+ROUND(AVG(wind_speed),3) AS montly_avg_speed
 FROM 
     wind_sites.upd_wind_site
 GROUP BY
-	TO_CHAR(date_time, 'YYYY-FMMonth'),
+	TO_CHAR(date_time, 'YYYY-FMMonth')
 ORDER BY
 		year_month
