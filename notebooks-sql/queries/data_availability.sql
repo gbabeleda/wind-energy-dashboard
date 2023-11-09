1702189c23a6ed33da118dc017d9b9166eac994f
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-11-09 13:31:15 +0800

    removed uneccesary comments data_avail

--- notebooks-sql/queries/data_availability.sql
+++ notebooks-sql/queries/data_availability.sql
@@ -12,22 +12,3 @@ FROM
     wind_sites.upd_wind_site
 GROUP BY 1,2, TO_CHAR(date_time, 'YYYY-FMMonth')
 ;
-
--- CREATE OR REPLACE VIEW wind_sites.data_availability AS
--- SELECT 
---     a.year,
---     a.month,
---     COUNT(DISTINCT a.day)
--- FROM 
---     (
---         SELECT
---             EXTRACT(YEAR FROM date_time) AS year,
---             EXTRACT(MONTH FROM date_time) AS month,
---             EXTRACT(DAY FROM date_time) AS day
---         FROM
---             wind_sites.upd_wind_site AS upd
---     ) AS a
--- GROUP BY year, month
--- ;
-
--- DROP VIEW wind_sites.data_availability;
