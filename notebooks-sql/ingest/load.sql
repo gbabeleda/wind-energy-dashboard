5b946b3ef692db06a0f3794f833b2872cf92aaa9
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-11-21 00:25:12 +0800

    Removed SQL queries will add to create view later

--- notebooks-sql/ingest/load.sql
+++
@@ -1,4 +0,0 @@
-COPY wind_sites.upd_wind_site (date_time,wind_speed,gust_speed,wind_direction)
-FROM '/Users/gioabeleda/Desktop/wind-energy-dashboard-streamlit/data/wind_energy.csv'
-DELIMITER ','
-CSV HEADER;
