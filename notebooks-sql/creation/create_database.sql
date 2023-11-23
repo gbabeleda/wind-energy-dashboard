6fa76484bb70203bc9f47dbf77fdc431b4ba8d69
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-11-24 00:54:47 +0800

    cleanup

--- notebooks-sql/creation/create_database.sql
+++
@@ -1,6 +0,0 @@
--- To do this
--- Connect to the default pre-existing postgres database on the Postgresql server
--- using SQLtools. Run this query. 
--- Disconnect, connect to the new database using SQLtools
-CREATE DATABASE wind_energy;
-
