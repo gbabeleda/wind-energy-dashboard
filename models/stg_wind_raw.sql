6f38d4ae7c04b5b8b5715ec2632d3a8e1726eeee
Gio Abeleda <gioabeleda@gmail.com>
2023-11-24 11:34:22 +0000

    First try loading data into dbt

---
+++ models/stg_wind_raw.sql
@@ -0,0 +1,16 @@
+WITH source_data AS (
+    SELECT 
+        _GMT_08_00_ AS date_time,
+        Wind_Speed__m_s AS wind_speed,
+        Gust_Speed__m_s AS gust_speed,
+        Wind_Direction____ AS wind_direction
+    FROM {{source("wind_energy_raw", "upd_wind_site")}}
+)
+
+SELECT 
+    date_time,
+    wind_speed,
+    gust_speed,
+    wind_direction
+FROM source_data
+-- WHERE wind_speed IS NOT NULL AND gust_speed IS NOT NULL AND wind_direction IS NOT NULL
