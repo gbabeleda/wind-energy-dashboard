c2bfac7229eeba1cd5e447637961a902601a3a77
Gio Abeleda <gioabeleda@gmail.com>
2023-11-24 11:43:23 +0000

    stg_wind_raw final

--- models/stg_wind_raw.sql
+++ models/stg_wind_raw.sql
@@ -1,13 +1,18 @@
+-- Rename columns
 WITH source_data AS (
     SELECT 
-        _GMT_08_00_ AS date_time,
-        Wind_Speed__m_s AS wind_speed,
-        Gust_Speed__m_s AS gust_speed,
-        Wind_Direction____ AS wind_direction
+        datetime AS date_time,
+        windspeed AS wind_speed,
+        gustspeed AS gust_speed,
+        winddirection AS wind_direction
     FROM {{source("wind_energy_raw", "upd_wind")}}
+    ORDER BY date_time
+),
+cleaned_data AS (
+    SELECT *
+    FROM source_data
+    WHERE wind_speed IS NOT NULL AND gust_speed IS NOT NULL AND wind_direction IS NOT NULL
 )
-
 SELECT 
     *
-FROM source_data
--- WHERE wind_speed IS NOT NULL AND gust_speed IS NOT NULL AND wind_direction IS NOT 
+FROM cleaned_data
