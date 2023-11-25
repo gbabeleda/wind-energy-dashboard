de624ee366d1bf9731832c138c4bdbeace2bb94c
Gio Abeleda <gioabeleda@gmail.com>
2023-11-25 08:10:40 +0000

    Changed reference to feature_wind_hourly. Applied dbt style

--- models/diurnal_variation_yearly.sql
+++ models/diurnal_variation_yearly.sql
@@ -1,15 +1,19 @@
-WITH feature_wind AS (
-    SELECT * FROM {{ ref('feature_wind') }}
+WITH feature_wind_hourly AS (
+    SELECT * FROM {{ ref('feature_wind_hourly') }}
+), 
+
+diurnal_variation_yearly AS (
+    SELECT 
+        years,
+        hours,
+        
+        ROUND(AVG(avg_wind_speed),3) AS avg_wind_speed
+
+    FROM feature_wind_hourly
+
+    GROUP BY 1, 2
 )
 
-SELECT 
-    years,
-    hours,
-    
-    ROUND(AVG(wind_speed),3) AS avg_wind_speed
-
-FROM feature_wind
-
-GROUP BY 1, 2
+SELECT * FROM diurnal_variation_yearly
 
 ORDER BY 1, 2
