5eaaabaaa74b5de1ca60a89468e7afa5da9d1871
Gio Abeleda <gioabeleda@gmail.com>
2023-11-25 03:20:11 +0000

    Turned feature_wind into a table

--- models/feature_wind.sql
+++ models/feature_wind.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 {# Feature Engineering (?) #}
 SELECT 
     EXTRACT(YEAR FROM date_time) AS years,
@@ -5,6 +7,8 @@ SELECT
     FORMAT_TIMESTAMP('%Y-%B', date_time) as year_month,
     EXTRACT(DAY FROM date_time) AS days,
     EXTRACT(HOUR FROM date_time) + 1 AS hours,
+
     wind_speed,
     wind_direction
+    
 FROM {{ ref('stg_wind_raw') }}
