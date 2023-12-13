3d62de41adfb78f9b80148a85cadeae052d831e2
Jose Mari Angelo Abeleda <gioabeleda@gmail.com>
2023-12-13 15:00:01 +0800

    Initial commit streamlit multipage

---
+++ streamlit/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+st.title("Wind Resource Assessment Dashboard")
+
+# Create API client
+gcp_credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+client = bigquery.Client(credentials=gcp_credentials)
+
+# data_availability = client.query("select * from `wired-ripsaw-403910.dbt_gbabeleda.data_availability`").to_dataframe()
+
